54d64992822018c9668a7335adb8f5beb0343472
Luqman Bello <bellolukman007@gmail.com>
2019-02-28 21:56:18 +0100

    seperated middle ware from vendors

--- routes/vendors/index.js
+++ routes/vendors/index.js
@@ -8,7 +8,7 @@ const cookieParser = require('cookie-parser')
 const uniqid = require('uniqid')
 const paystack = require('../../paystack-config')
 const validator = require('../../schemas/object')
-
+const middleware = require('./middleware')
 router.use(
   bodyParser.urlencoded({
     extended: true
@@ -18,105 +18,8 @@ router.use(bodyParser.json())
 
 router.use(cookieParser(keys.cookie))
 
-function isEmptyObj (obj) {
-  let isEmpty = true
-  for (let key in obj) {
-    if (Object.prototype.hasOwnProperty.call(obj, key)) {
-      isEmpty = false
-    }
-  }
-  return isEmpty
-}
-function authorize (req, res, next) {
-  console.log('--------Verify middleware---------')
-  const cookies = req.signedCookies
-  if (isEmptyObj(cookies) && req.body.token == null) {
-    res.status(403).send({
-      state: 'Forbidden'
-    })
-    console.log('-----------No cookies----------')
-  } else {
-    const bearerToken = cookies.token ? cookies.token : req.body.token
-    delete req.body.token
-    jwt.verify(bearerToken, keys.jwt, (err, authData) => {
-      if (err) {
-        res.status(500).send({
-          state: 'ERROR',
-          payload: err
-        })
-        return
-      }
-      req.sender = authData
-      next()
-    })
-  }
-}
-function exists (req, res, next) {
-  req.db.createCollection('Vendors', function (err, collection) {
-    if (err) throw res.status(500).send(err.toString())
-    collection.findOne({ account_number: req.body.account_number }, function (
-      err,
-      ven
-    ) {
-      if (err) throw res.status(500).send(err)
-      if (ven === null) {
-        next()
-      } else {
-        res.json({
-          state: 'VENDOR EXISTS',
-          payload: ven
-        })
-      }
-    })
-  })
-}
-function isEmail (email) {
-  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(String(email).toLowerCase())
-}
-function validate (req, res, next) {
-  console.log("---- VALIDATE-----")
-  if (!isEmail(req.body.email)) {
-    res.status(400).send({
-      state: 'INVALID EMAIL'
-    })
-    return;
-  }
-  request(
-    {
-      url: `https://api.paystack.co/bank/resolve?account_number=${
-        req.body.account_number
-      }&bank_code=${req.body.bank_code}`,
-      method: 'GET',
-      jar: true,
-      headers: {
-        Authorization: 'Bearer ' + paystack.secret
-      }
-    },
-    function (err, response, body) {
-      console.log(body)
-      if (err) throw res.status(500).send(err)
-      let data = JSON.parse(body)
-      if (response.statusCode === 200) {
-        if (data.status && data.message === 'Account number resolved') {
-          req.body.account_name = data.data.account_name
-          next()
-        } else {
-          res.status(400).send({
-            state: data['message']
-          })
-        }
-      } else {
-        res.status(400).send({
-          state: data['message']
-        })
-      }
-    }
-    
-  )
-}
 
-router.get('/', authorize, function (req, res) {
+router.get('/', middleware.authorize, function (req, res) {
   req.db.createCollection('Vendors', function (err, collection) {
     if (err) throw res.status(500).send(err)
     collection.find({}, { password: 0 }).toArray(function (err, sigs) {
@@ -129,7 +32,7 @@ router.get('/', authorize, function (req, res) {
   })
 })
 
-router.post('/', [authorize, exists, validate], function (req, res) {
+router.post('/', [middleware.authorize, middleware.exists, middleware.validate], function (req, res) {
   let newVendor = req.body
   newVendor._id = uniqid('ven-')
   let rules = [
@@ -215,7 +118,7 @@ router.post('/', [authorize, exists, validate], function (req, res) {
   )
 })
 
-router.post('/:id', authorize, function (req, res) {
+router.post('/:id', middleware.authorize, function (req, res) {
   let rules = [
     {
       key: 'vendor_name',
---
+++ routes/vendors/middleware.js
@@ -0,0 +1,106 @@
+
+function isEmptyObj (obj) {
+    let isEmpty = true
+    for (let key in obj) {
+      if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        isEmpty = false
+      }
+    }
+    return isEmpty
+  }
+  function authorize (req, res, next) {
+    console.log('--------Verify middleware---------')
+    const cookies = req.signedCookies
+    if (isEmptyObj(cookies) && req.body.token == null) {
+      res.status(403).send({
+        state: 'Forbidden'
+      })
+      console.log('-----------No cookies----------')
+    } else {
+      const bearerToken = cookies.token ? cookies.token : req.body.token
+      delete req.body.token
+      jwt.verify(bearerToken, keys.jwt, (err, authData) => {
+        if (err) {
+          res.status(500).send({
+            state: 'ERROR',
+            payload: err
+          })
+          return
+        }
+        req.sender = authData
+        next()
+      })
+    }
+  }
+  function exists (req, res, next) {
+    req.db.createCollection('Vendors', function (err, collection) {
+      if (err) throw res.status(500).send(err.toString())
+      collection.findOne({ account_number: req.body.account_number }, function (
+        err,
+        ven
+      ) {
+        if (err) throw res.status(500).send(err)
+        if (ven === null) {
+          next()
+        } else {
+          res.json({
+            state: 'VENDOR EXISTS',
+            payload: ven
+          })
+        }
+      })
+    })
+  }
+  function isEmail (email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(String(email).toLowerCase())
+  }
+  function validate (req, res, next) {
+    console.log("---- VALIDATE-----")
+    if (!isEmail(req.body.email)) {
+      res.status(400).send({
+        state: 'INVALID EMAIL'
+      })
+      return;
+    }
+    request(
+      {
+        url: `https://api.paystack.co/bank/resolve?account_number=${
+          req.body.account_number
+        }&bank_code=${req.body.bank_code}`,
+        method: 'GET',
+        jar: true,
+        headers: {
+          Authorization: 'Bearer ' + paystack.secret
+        }
+      },
+      function (err, response, body) {
+        console.log(body)
+        if (err) throw res.status(500).send(err)
+        let data = JSON.parse(body)
+        if (response.statusCode === 200) {
+          if (data.status && data.message === 'Account number resolved') {
+            req.body.account_name = data.data.account_name
+            next()
+          } else {
+            res.status(400).send({
+              state: data['message']
+            })
+          }
+        } else {
+          res.status(400).send({
+            state: data['message']
+          })
+        }
+      }
+      
+    )
+  }
+
+  const middleware = {
+      validate,
+      authorize,
+      exists
+  }
+
+  module.exports = middleware
